0ca099d527ad8f5f2c9ed90ba6c4f52e96eb0075
Yuge Zhang <scottyugochang@gmail.com>
2024-04-23 17:54:20 +0800

    debug

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {unoptimized: true},
     output: 'export',
     distDir: '../_site',
+    basePath: '/dseval',
 };
 
 export default nextConfig;
