9efdf776795a0c2f206e660f13a117d2becc8718
Yuge Zhang <scottyugochang@gmail.com>
2024-04-23 18:03:24 +0800

    revert

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {unoptimized: true},
     output: 'export',
     distDir: '../_site',
-    basePath: '/dseval',
 };
 
 export default nextConfig;
